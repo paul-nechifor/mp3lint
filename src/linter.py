12d4cd495c80b213ae109a428e32a4b0a28e2738
Paul Nechifor <paul@nechifor.net>
2016-03-27 22:32:45 +0100

    Add an empty directory checker.

--- src/linter.py
+++ src/linter.py
@@ -1,4 +1,7 @@
+import json
 import os
+
+from checkers import checker_classes
 
 
 class Linter(object):
@@ -6,10 +9,14 @@ class Linter(object):
     def __init__(self, library_path):
         self.library_path = os.path.abspath(library_path)
         self.structure = None
+        self.checkers = [x(self) for x in checker_classes]
+        self.problems = []
 
     def run(self):
         self.structure = self.load_files()
-        print self.structure
+        for x in self.checkers:
+            x.run()
+        print json.dumps(self.problems, indent=2)
 
     def load_files(self):
         ret = {}
@@ -17,14 +24,15 @@ class Linter(object):
         for root, dirs, files in os.walk(self.library_path):
             sub = get_dict(ret, os.path.relpath(root, self.library_path))
             for file in files:
-                sub[file] = {'type': 'file', 'name': file}
-            for dir in dirs:
-                sub[dir] = {'type': 'dir', 'name': dir}
+                sub[file] = None
 
         return ret
 
 
+
 def get_dict(d, path):
+    if path == '.':
+        return d
     parts = path.split('/')
     ret = d
     for part in parts:
