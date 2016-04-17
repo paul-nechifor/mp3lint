d0643a3cf8ea51c910b982ee7cdd54d4c5034d18
Paul Nechifor <paul@nechifor.net>
2016-04-17 17:05:05 +0100

    Say which dir will be scanned.

--- mp3lint/linter.py
+++ mp3lint/linter.py
@@ -1,6 +1,14 @@
+from .config import read_or_gen_config
+
+
 class Linter(object):
     def __init__(self):
-        pass
+        self.config = {}
+
+    def load(self):
+        self.config = read_or_gen_config()
 
     def run(self):
+        self.load()
+        print('Scanning "%s"...' % self.config['music_dir'])
         print('Done.')
--- mp3lint/test_linter.py
+++ mp3lint/test_linter.py
@@ -1,11 +1,18 @@
 from .linter import Linter
 from .test_utils import TestCase
 
+from mock import patch
+
 
 class TestLinter(TestCase):
 
-    def test_run_prints_done(self):
+    @patch('mp3lint.linter.read_or_gen_config')
+    def test_run_prints_done(self, mock):
+        mock.return_value = {'music_dir': '/asdf/usr'}
         with self.capture_output() as stringio:
             Linter().run()
-        value = stringio.getvalue()
-        self.assertEqual('Done.\n', value)
+        output = (
+            'Scanning "/asdf/usr"...\n'
+            'Done.\n'
+        )
+        self.assertEqual(output, stringio.getvalue())
